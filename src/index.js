a99fc913ec834536b8b45454fa1165c15d3b93ba
kanishths <kanishthasiyaram15@gmail.com>
2021-07-31 15:06:16 +0530

    user list added

--- src/index.js
+++ src/index.js
@@ -16,15 +16,22 @@ app.use(express.static(publicDirectoryPath));
 
 io.on("connection", (socket) => {
   console.log("New Connection");
-  socket.emit("message", generateMessage("Welcome!"));
-  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
+
+  socket.on("join", ({ username, room }) => {
+    socket.join(room);
+
+    socket.emit("message", generateMessage("Welcome!"));
+    socket.broadcast
+      .to(room)
+      .emit("message", generateMessage(`${username} has joined the chat!`));
+  });
 
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
     if (filter.isProfane(message)) {
       return callback("profanity is not allowed");
     }
-    io.emit("message", generateMessage(message));
+    io.to("1234").emit("message", generateMessage(message));
     callback();
   });
 
